63e44a81c5a9fc7e7a4a167f9427686ad2025be2
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-28 00:56:33 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,12 +9,12 @@ end
  puts introduction("Josh")
  
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and i am learning to program #{language}."
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
 end 
 puts introduction_with_language("Dan", "Ember.js")
 
 def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is #{name} and i am learning to program #{language}."
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
 end 
 puts introduction_with_language_optional("Edwin", "Ruby")
   
